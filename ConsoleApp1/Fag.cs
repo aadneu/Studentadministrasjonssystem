263c3200819af8d31560e3ee1bb99f64fa2d301c
aadneu <141396377+aadneu@users.noreply.github.com>
2023-11-26 13:38:21 +0100

    Add project files.

---
+++ ConsoleApp1/Fag.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Fag
+    {
+        private String Fagkode;
+        private string Fagnavn;
+        private int AntallStudiepoeng;
+
+        public Fag(string fagkode, string fagnavn, int antallStudiepoeng)
+        {
+            Fagkode = fagkode;
+            Fagnavn = fagnavn;
+            AntallStudiepoeng = antallStudiepoeng;
+        }
+
+        void SkrivUtInfo()
+        {
+            Console.WriteLine($"Fagkode: {Fagkode}" +
+                              $"Fagnavn: {Fagnavn}" +
+                              $"Antall studiepoeng: {AntallStudiepoeng}");
+        }
+    }
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Student
+    {
+        private string Name;
+        private int Alder;
+        private string Studieprogram;
+        private int StudentID;
+
+        public Student(string name, int alder, string studieprogram, int studentId)
+        {
+            Name = name;
+            Alder = alder;
+            Studieprogram = studieprogram;
+            StudentID = studentId;
+        }
+
+        void SkrivUtInfo()
+        {
+            Console.WriteLine($"Navn: {Name}" +
+                              $"Alder: {Alder}" +
+                              $"Studieprogram: {Studieprogram}" +
+                              $"StudentID: {StudentID}");
+        }
+    }
+}
